64c711245ec66e3086dbd4e025c0997f07cf94bd
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-11-19 15:22:05 +1100

    an object of unique words as keys and their combinations as an array of values

--- markov/markov_chan.js
+++ markov/markov_chan.js
@@ -6,20 +6,36 @@
 - certain sequences appear more than others
 - spit out new text based on probabilities 
 */
+const text = "this rainbow has a unicorn flying over it this rainbow has a unicorn flying over it this is it chaps the big one markos the big gun run mum fun run bun wow";
 
-const nGrams = {};
-const text = "this rainbow has a unicorn flying over it this rainbow has a unicorn flying over it";
-const order = 3;
-for (let i = 0; i <= text.length / (order * 2); i++) {
-    const textArray = text.split(" ")
-    const gram = textArray.slice(i, i + order);
-    //nGrams[i] = gram
-    if (!nGrams[gram]) {
-        nGrams[gram] = [];
-        nGrams[gram].push(gram.toString())
-    } else {
-        nGrams[gram].push(gram.toString())
-        //nGrams[gram]++
+function nGrams(text) {
+    const nGrams = [];
+    const order = 2;
+    for (let i = 0; i <= (text.length - 1) / (order * 2); i++) {
+        const textArray = text.split(" ")
+        const gram = textArray.slice(i, i + order);
+        nGrams.push(gram.join(" "))
     }
+    return nGrams
 }
-console.log(nGrams)
+let nGramsRes = nGrams(text)
+
+function nGramsOrganised(text, nGrams) {
+    let orderedGrams = {}
+    const uniqueWords = new Set(text.split(" "))
+    let wordsCombos = [];
+
+    uniqueWords.forEach(word => {
+        let wordCombos = []
+        nGrams.map(nGram => {
+            nGramArr = nGram.split(" ")
+            if (nGramArr[0] == word) {
+                wordCombos.push(nGram)
+            }
+        })
+        wordsCombos.push(wordCombos)
+        orderedGrams[word] = wordCombos
+    })
+    return orderedGrams
+}
+console.log(nGramsOrganised(text, nGramsRes))
