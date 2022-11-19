d440ac2cc8f27494aa9ef3974cdedeae9064b5d6
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-11-19 12:40:01 +1100

    nGrams collection is object with iterative keys

--- markov/markov_chan.js
+++ markov/markov_chan.js
@@ -7,12 +7,17 @@
 - spit out new text based on probabilities 
 */
 
-const nGrams = [];
+const nGrams = {};
 const text = "this rainbow has a unicorn flying over it";
 const order = 3;
 for (let i = 0; i <= text.length / (order * 2); i++) {
     const textArray = text.split(" ")
     const gram = textArray.slice(i, i + order);
-    nGrams.push(gram)
+    nGrams[i] = gram
+    if (!nGrams[gram]) {
+        nGrams[gram] == 1
+    } else {
+        nGrams[gram]++
+    }
 }
 console.log(nGrams)
