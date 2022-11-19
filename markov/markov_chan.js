b10a60d1354950dd460937bd0cf8068acac5e92e
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-11-19 12:23:59 +1100

    array of ngrams

---
+++ markov/markov_chan.js
@@ -0,0 +1,18 @@
+/*
+- read text
+- or at least have text to go over
+- words are states, some follow another
+- some appear more than others
+- certain sequences appear more than others
+- spit out new text based on probabilities 
+*/
+
+const nGrams = [];
+const text = "this rainbow has a unicorn flying over it";
+const order = 3;
+for (let i = 0; i <= text.length / (order * 2); i++) {
+    const textArray = text.split(" ")
+    const gram = textArray.slice(i, i + order);
+    nGrams.push(gram)
+}
+console.log(nGrams)
