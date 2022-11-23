38d8955b1b0f2c7495634e7f8bca22cadf2adfe4
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-11-23 21:05:22 +1100

    ignorant generative text implementation

--- markov/markov_chan.js
+++ markov/markov_chan.js
@@ -1,6 +1,4 @@
 /*
-- read text
-- or at least have text to go over
 - words are states, some follow another
 - some appear more than others
 - certain sequences appear more than others
@@ -25,32 +23,45 @@ function nGramsFunc(text, num) {
     for (let i = 0; i <= (text.length - 1) / (order * 2); i++) {
         const textArray = text.split(" ")
         const gram = textArray.slice(i, i + order);
-        nGrams.push(gram.join(" "))
+        if (gram.length > 0) {
+            nGrams.push(gram.join(" "))
+        }
     }
-    return nGramsOrganised(text, nGrams)
+    return newWords(nGrams, 200)
 }
 
 
-function nGramsOrganised(text, nGrams) {
-    let orderedGrams = {}
-    text = text.split(" ") // only now is text mutated 
-    let wordsCombos = [];
+// function nGramsOrganised(text, nGrams) {
+//     let orderedGrams = {}
+//     text = text.split(" ") // only now is text mutated 
+//     let wordsCombos = [];
 
-    text.map(word => {
-        let wordCombos = []
-        nGrams.map(nGram => {
-            nGramArr = nGram.split(" ")
-            if (nGramArr[0] == word) { // because it's passed nGrams of two words each, the first word is therefore the target i.e. [0]
-                wordCombos.push(nGram)
-            }
-        })
-        wordsCombos.push(wordCombos)
-        orderedGrams[word] = wordCombos
-    })
-    console.log(orderedGrams)
-    return orderedGrams
+//     text.map(word => {
+//         let wordCombos = []
+//         nGrams.map(nGram => {
+//             nGramArr = nGram.split(" ")
+//             if (nGramArr[0] == word) { // because it's passed nGrams of two words each, the first word is therefore the target i.e. [0]
+//                 wordCombos.push(nGram)
+//             }
+//         })
+//         wordsCombos.push(wordCombos)
+//         orderedGrams[word] = wordCombos
+//     })
+//     console.log(orderedGrams)
+//     return orderedGrams
+// }
+
+// now make the orderedgrams spit out new text!!
+
+function newWords(nGrams, amount) {
+    let generatedText = [];
+    for (let i = 0; i < amount; i++) {
+        let num = Math.ceil(Math.random() * nGrams.length - 1)
+        let nGram = nGrams[num]
+        generatedText.unshift(nGram)
+    }
+    console.log(generatedText.join(" "))
 }
-
 /*
 - if i do something like for each  key in the obj returned from nGrams(text, 2)
 - find the n number of things that typically come after it in text then we're really marko chaining
